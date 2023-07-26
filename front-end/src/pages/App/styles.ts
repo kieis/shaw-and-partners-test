5a0be9146fa7266315fb39c77964ca09dfae3637
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-07-26 00:40:44 -0300

    feat: app upload csv handle

--- front-end/src/pages/App/styles.ts
+++ front-end/src/pages/App/styles.ts
@@ -1,13 +1,23 @@
 import { styled, css } from "styled-components";
 
 export const Container = styled.div`
+    margin-inline: auto;
+
     background-color: ${({ theme }) => theme.colors.background };
     width: 100%;
-    max-width: 100%;
+    max-width: 144rem;
     height: auto;
     min-height: 100vh;
 
     padding: 2rem;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30%;
 `;
 
 export const CardList = styled.div`
@@ -21,7 +31,7 @@ export const CardList = styled.div`
 
 export const Text = styled.span`
     ${({ theme }) => css`
-        font-size: ${theme.font_size.md}px;
+        font-size: ${theme.font_size.md}rem;
         color: ${theme.colors.text_primary};
     `};
 
